e8463412d9148114adae4216939188bc5ecdc836
TGA88 <supawut.thomas@gmail.com>
2021-10-21 17:52:47 +0700

    finish implement stepper

--- src/common/UI/stepper/index.js
+++ src/common/UI/stepper/index.js
@@ -1,9 +1,26 @@
 import React, { useState } from "react";
 
 function Stepper() {
-  return (<div>
-      Stepper component
-  </div>)
+  const [score, setScore] = useState(0);
+
+  function increase(e) {
+    setScore(score + 1);
+  }
+
+  function decrease(val) {
+
+    setScore(score - 1);
+  }
+
+  return (
+    <div>
+      <div>
+        <span data-testid='txtScore'>{score}</span>
+      </div>
+      <button onClick={increase} >+</button>
+      <button onClick={decrease} >-</button>
+    </div>
+  )
 }
 
 
